4ef7848a13ab4c24674f4387b24bc2deea865ca9
AS <asimasbbajwa@gmail.com>
2026-08-09 16:59:17 +0100

    Added Splash Screen

--- app/src/main/java/com/asdroid/jetpack_ui/splash/ui/theme/Theme.kt
+++ app/src/main/java/com/asdroid/jetpack_ui/splash/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.asdroid.jetpack_ui.splash.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
